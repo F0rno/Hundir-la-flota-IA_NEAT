ff87a5d8bf6df1f88a7c54749d30a687eaf31ba8
F0rno <you@example.com>
2023-01-19 08:20:03 +0100

    Añadido de la funcion algun_hundido

--- barcos.py
+++ barcos.py
@@ -12,10 +12,18 @@ def comprueba_barco(tablero, fila, columna, orientacion, posiciones):
     return True
 
 def coloca_barco(tablero, fila, columna, orientacion, posiciones):
+    coordenada  = []
+    coordenadas = []
     for i in range(posiciones):
         tablero[fila+i*orientacion][columna+i*(orientacion==0)] = str(posiciones)
+        coordenada.append((fila+i*orientacion, columna+i*(orientacion==0)))
+        #print(f"{posiciones}: fila: {fila+i*orientacion}, columna: {columna+i*(orientacion==0)}")
+    #print()
+    coordenadas.append(coordenada)
+    return tuple(coordenadas)
 
 def coloca_barcos(tablero, barcos):
+    coordenas_de_barcos = ()
     for numero_barcos, posiciones in barcos:
         for _ in range(numero_barcos):
             while True:
@@ -24,12 +32,19 @@ def coloca_barcos(tablero, barcos):
                 orientacion = random.randint(0,1)
                 if comprueba_barco(tablero, fila, columna, orientacion, posiciones):
                     break
-            coloca_barco(tablero, fila, columna, orientacion, posiciones)
+            coordenas_de_barcos += coloca_barco(tablero, fila, columna, orientacion, posiciones)
+    return coordenas_de_barcos
 
+def algun_hundido(coordenas_de_barcos):
+    for coordenadas in coordenas_de_barcos:
+        if len(coordenadas) == 0:
+            return True
+    return False
 
 if __name__ == "__main__":
     barcos = ((1, 4), (2, 3), (3, 2), (4, 1))
 
     miTablero = genera_tablero()
-    coloca_barcos(miTablero, barcos)
+    coordenas_de_barcos = coloca_barcos(miTablero, barcos)
+    algun_hundido(coordenas_de_barcos)
     imprimir_tablero(miTablero)
