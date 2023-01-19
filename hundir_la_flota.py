6880d3565e1f8ffa10a06d9c0d821552e19f0df5
F0rno <you@example.com>
2023-01-19 09:59:33 +0100

    Cambio en la funcion algun_hundido, ahora elimina posiciones de barcos pasados por referencia si las coordenadas pasadas a la función, colisionan con un barco existente

--- hundir_la_flota.py
+++ hundir_la_flota.py
@@ -2,10 +2,12 @@ from tablero import genera_tablero, imprimir_tablero
 from barcos import coloca_barcos
 import random
 
-def algun_hundido(coordenadas_de_barcos):
+def algun_hundido(fila, columna, coordenadas_de_barcos):
     for coordenadas in coordenadas_de_barcos:
         if len(coordenadas) == 0:
             return True
+        if (fila, columna) in coordenadas:
+            coordenadas.remove((fila, columna))
     return False
 
 def elegir_coordenada():
@@ -13,13 +15,13 @@ def elegir_coordenada():
     columna = random.randint(0,9)
     return fila, columna
 
-def dispara(fila, columna, tablero_enemigo):
+def dispara(fila, columna, tablero_enemigo, coordenadas_de_barcos_jugador):
     if tablero_enemigo[fila][columna] == " ":
         return "A"
     if tablero_enemigo[fila][columna] != " ":
+        if algun_hundido(fila, columna,coordenadas_de_barcos_jugador):
+            return "H"
         return "T"
-    if algun_hundido:
-        return "H"
 
 if __name__ == "__main__":
     barcos = ((1, 4), (2, 3), (3, 2), (4, 1))
@@ -47,10 +49,10 @@ if __name__ == "__main__":
         
         # Dispara y retorna (Tocado, Hundido y Agua)
         if turno == 1:
-            resultado_del_disparo = dispara(fila, columna, jugador2_Tablero)
+            resultado_del_disparo = dispara(fila, columna, jugador2_Tablero, coordenadas_de_barcos_jugador2)
 
         if turno == 2:
-            resultado_del_disparo = dispara(fila, columna, jugador1_Tablero)
+            resultado_del_disparo = dispara(fila, columna, jugador1_Tablero, coordenadas_de_barcos_jugador1)
 
         # Imprime el tablero enemigo actualizado
         if resultado_del_disparo == "A":
@@ -75,10 +77,10 @@ if __name__ == "__main__":
 
             if turno == 2:
                 jugador2_TableroEnemigo[fila][columna] = "T"
-                contador_de_hundidos_juagor1 += 1
+                contador_de_hundidos_juagor2 += 1
                 print("Hundido!")
 
-        # Combrobar si hay ganador
+        # Combrobar si hay ganador, 10 gana
         
 
         # Cambio de turno
