ef1e1eddab003bc23c6fdbec4161fb260588cd44
F0rno <you@example.com>
2023-01-23 10:30:32 +0100

    Nueva condición en el test principal

--- test_hundir_la_flota.py
+++ test_hundir_la_flota.py
@@ -23,5 +23,8 @@ coordenadas_de_barcos_jugador1 = [[" ", " ", " ", " ", " ", " ", " ", " ", " ", 
 def test_algun_hundido():
     coordenadas_barcos = [[(1,1)], [(2,2)]]
     assert algun_hundido(1,1, coordenadas_barcos) == True
+    coordenadas_barcos = [[(1,1), (1,2)], [(2,2)]]
+    assert algun_hundido(1,1, coordenadas_barcos) == False
+
 
 
