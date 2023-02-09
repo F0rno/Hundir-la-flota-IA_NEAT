df031a3e83768088c8bef60c976d197a6a6846b3
F0rno <you@example.com>
2023-02-09 09:59:43 +0100

    Ya funciona el encuentro del juego y el envio de mensajes. Empieza el desarrollo del juego

--- hundir_la_flota_online.py
+++ hundir_la_flota_online.py
@@ -1,38 +1,51 @@
 from lanzar_escucha_ip import *
 from protocolo import *
 from time import sleep
+logging.basicConfig(level=logging.DEBUG)
 
-def jugar(socket_comunicacion: socket, es_server=False):
-    if not es_server:
-        print("Conectado, cerrando")
-        socket_comunicacion.close()
+def jugarComoServer(server_socket: socket):
+    message = b'Se acabaron los barquitos'
+    server_socket.sendall(message)
+    server_socket.close()
+
+def jugarComoCliente(server_cliente: socket):
+    data = server_cliente.recv(1024)
+    print(data)
+    server_cliente.close()
 
 def main():
+    IP = socket.gethostbyname(socket.gethostname())
     maximo_de_intentos = 5
 
     while (maximo_de_intentos <= 5):
-        print("Lanzando IP")
+        logging.debug("Lanzando IP")
+        # Bolean, Str
         ipLanzada = lanzarIP()
 
         if ipLanzada[0]:
             # Bolean, Str
             if ipLanzada[1] == "OK":
-                print("OK")
-                print("Abro socket TCP soy server")
-                
+                logging.debug("OK")
+                logging.debug("Abro socket TCP soy server")
+                hayCliente, socket_server = levantar_server(IP)
+                if hayCliente:
+                    jugarComoServer(socket_server)
+                else:
+                    logging.debug("No se a conectado ningun cliente")
+
         else:
             # Bolean, Socket
             escuchado = escucha()
             if escuchado[0]:
-                print("Recibo IP soy cliente")
-                print("Intento conectarme")
+                logging.debug("Recibo IP soy cliente")
+                logging.debug("Intento conectarme")
                 conexion, socket_server = conectar(escuchado[1])
                 if conexion:
                     # Lanza el juego con el 
                     # socket recibido para envir los paquetes
-                    jugar(socket_server)
+                    jugarComoCliente(socket_server)
                 else:
-                    print("No es posible conectarse")
+                    logging.debug("No es posible conectarse")
         maximo_de_intentos += 1
         break
 
--- protocolo.py
+++ protocolo.py
@@ -18,3 +18,42 @@ def conectar(ip):
         socket_con_server.close()
     return conexion, socket_con_server
 
+
+def levantar_server(ip):
+    # Crear un socket TCP
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.settimeout(4)
+    # Obtener la dirección IP y el puerto del servidor
+    server_address = (ip, 6969)
+    # Vincular el socket a la dirección y puerto del servidor
+    server_socket.bind(server_address)
+    # Escuchar solicitudes de conexión
+    server_socket.listen(1)
+    logging.debug('Servidor escuchando en {}:{}'.format(*server_address))
+    try:
+        logging.debug('Esperando una conexión...')
+        client_socket, client_address = server_socket.accept()
+        # Aceptar una conexión entrante
+        logging.debug('Conexión desde {}:{}'.format(*client_address))
+        conexion = True
+        return conexion, client_socket 
+    except Exception as err:
+        conexion = False
+        logging.debug(err)
+        server_socket.close()
+        return conexion, socket 
+       
+
+"""
+    Recibir datos del cliente
+    data = client_socket.recv(1024)
+
+    Enviar una respuesta al cliente
+    response = b'Mensaje recibido: ' + data
+    client_socket.sendall(response)
+
+    Cerrar la conexión con el cliente
+    client_socket.close()
+    break
+
+"""
